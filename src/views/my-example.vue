054a117232c6cfba667efe30172c445af0c23aba
v <a@tok7.com>
2021-06-06 20:37:34 +0800

    :art: reduce  例子

--- src/views/my-example.vue
+++ src/views/my-example.vue
@@ -49,8 +49,45 @@
         6. 使用扩展运算符和initialValue绑定包含在对象数组中的数组
       </h4>
       <div class="example-container">
-        <p>[[0, 1], [2, 3], [4, 5]]</p>
+        <p>
+          { name: "Anna", books: ["Bible", "Harry Potter"], age: 21, }, { name:
+          "Bob", books: ["War and peace", "Romeo and Juliet"], age: 26, }, {
+          name: "Alice", books: ["The Lord of the Rings", "The Shining"], age:
+          18, },
+        </p>
         <p>{{ q6 }}</p>
+      </div>
+    </section>
+
+    <section>
+      <h4 class="text-align">7. 数组去重</h4>
+      <div class="example-container">
+        <p>['a', 'b', 'a', 'b', 'c', 'e', 'e', 'c', 'd', 'd', 'd', 'd']</p>
+        <p>{{ q7 }}</p>
+      </div>
+    </section>
+
+    <section>
+      <h4 class="text-align">8. 按顺序运行Promise</h4>
+      <div class="example-container">
+        <p></p>
+        <p>{{ q8 }}</p>
+      </div>
+    </section>
+
+    <section>
+      <h4 class="text-align">9.功能型函数管道</h4>
+      <div class="example-container">
+        <p></p>
+        <p>{{ q9 }}</p>
+      </div>
+    </section>
+
+    <section>
+      <h4 class="text-align">10. 使用 reduce实现map</h4>
+      <div class="example-container">
+        <p></p>
+        <p>{{ q10 }}</p>
       </div>
     </section>
   </div>
@@ -102,7 +139,7 @@ export default class Home extends Vue {
     return c;
   }
 
-  get q5(): Record<string, number | string>[] {
+  get q5(): any {
     let people: {
       name: string;
       age: number;
@@ -112,11 +149,21 @@ export default class Home extends Vue {
       { name: "Max", age: 20, sex: 1 },
       { name: "Jane", age: 20, sex: 0 },
     ];
-
-    return people;
+    function groupBy(objAry: any[], property: string): any {
+      return objAry.reduce((ac, cu) => {
+        let key = cu[property];
+        if (!ac[key]) {
+          ac[key] = [];
+        }
+        ac[key].push(cu);
+        return ac;
+      }, {});
+    }
+    let result = groupBy(people, "age");
+    return result;
   }
 
-  get q6() {
+  get q6(): string[] {
     let friends = [
       {
         name: "Anna",
@@ -134,7 +181,20 @@ export default class Home extends Vue {
         age: 18,
       },
     ];
-    return friends;
+    let a = (ac: string[], cu: any) => [...ac, ...cu.books];
+    let b: string[] = friends.reduce(a, []);
+    return b;
+  }
+
+  get q7(): number {
+    return 1;
+  }
+
+  get q8(): number {
+    return 2;
+  }
+  get q9(): number {
+    return 3;
   }
   created(): void {
     console.log(998);

69fe1714f845b3e4dad83932888a7845514543f5
v <a@tok7.com>
2021-06-13 01:41:02 +0800

    :art: keep-alive tabs

---
+++ src/components/TabContent.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="app-container">
+    <div class="user-tab">
+      <ul class="user-sidebar">
+        <li v-for="item in lib" :key="item" @click="selectedUser = item">
+          {{ item.name }}
+        </li>
+      </ul>
+      <div class="selected-user-container">
+        <div class="selected-user" v-if="selectedUser">
+          <h2>
+            {{ selectedUser.name }}
+          </h2>
+
+          <p>
+            {{ selectedUser.content }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+@Options({})
+export default class Home extends Vue {
+  private selectedUser = "";
+  private lib = [
+    {
+      id: 1,
+      name: "波塞冬",
+      content: "ssss",
+    },
+    {
+      id: 2,
+      name: "雅典娜",
+      content: "ssss222",
+    },
+  ];
+}
+</script>
+<style lang="less" scoped>
+.app-container {
+  width: 777px;
+}
+.user-tab {
+  display: flex;
+}
+.selected-user-container {
+  height: 233px;
+  width: 333px;
+}
+</style>
--- src/views/keep-alive.vue
+++ src/views/keep-alive.vue
@@ -1,25 +1,40 @@
 <template>
   <div class="home">
+    <div class="app-container">
+      <button v-for="item in tabs" :key="item" @click="currentTab = item">
+        {{ item }}
+      </button>
+      <div class="tab-container">
+        <keep-alive>
+          <component :is="currentTabComponent"></component>
+        </keep-alive>
+      </div>
+    </div>
     <button @click="add">+</button>
     {{ c }}
     <button @click="handleTo">跳转</button>
-    <keep-alive>
-      <count ref="count" :count="c" />
-    </keep-alive>
+    <count ref="count" :count="c" />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import Count from "./TestKeepAlive.vue";
+import TabUser from "@/components/TabContent.vue";
 @Options({
   components: {
     Count,
+    TabUser,
   },
 })
 export default class Home extends Vue {
+  private tabs = ["user", "other"];
+  private currentTab = "";
   private c = 0;
 
+  get currentTabComponent(): string {
+    return "tab-" + this.currentTab.toLowerCase();
+  }
   private handleTo() {
     this.$router.push({
       name: "Example",
@@ -31,3 +46,16 @@ export default class Home extends Vue {
   }
 }
 </script>
+<style lang="less" scoped>
+.app-container {
+  height: 444px;
+  background: aqua;
+
+  .tab-container {
+    height: 333px;
+    width: 222px;
+    background: salmon;
+    margin: 0 auto;
+  }
+}
+</style>

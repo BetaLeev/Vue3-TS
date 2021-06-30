cca7adadb26ab634a1862d996598f0c591ce4517
v <a@tok7.com>
2021-07-01 00:18:35 +0800

    :wrench: 新增 webpack 配置

---
+++ dist/precache-manifest.477592ffc9d8888fba5f924618ce9fdd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2f12f808f54bc277a3",
+    "url": "/css/app.4d6d32d5.css"
+  },
+  {
+    "revision": "b4465b817956bab361ac",
+    "url": "/css/chunk-e877a220.4d1a5785.css"
+  },
+  {
+    "revision": "1b5f21a14403dfacf04c878f2d322585",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e19f0651535090b92acb",
+    "url": "/js/about.ea2cf823.js"
+  },
+  {
+    "revision": "7c2f12f808f54bc277a3",
+    "url": "/js/app.07d73753.js"
+  },
+  {
+    "revision": "5ff6f2784017744da438",
+    "url": "/js/chunk-2d0b2798.0b99ff6b.js"
+  },
+  {
+    "revision": "e9cce93f6cad7291fb1f",
+    "url": "/js/chunk-2d0d0471.59a91a2e.js"
+  },
+  {
+    "revision": "b4465b817956bab361ac",
+    "url": "/js/chunk-e877a220.5def8229.js"
+  },
+  {
+    "revision": "daae5d58b5305f1ce14c",
+    "url": "/js/chunk-vendors.ea8e5e86.js"
+  },
+  {
+    "revision": "27b1ab3fec5c4c6e5b08437fcc4becdc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ vue.config.ts
@@ -0,0 +1,54 @@
+import OptimizeCssAssetsPlugin from "optimize-css-assets-webpack-plugin";
+import UglifyJsPlugin from "uglifyjs-webpack-plugin";
+import HappyPack from "happypack";
+const BundleAnalyzerPlugin =
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            warnings: false,
+            drop_console: true, //console
+            drop_debugger: true,
+            pure_funcs: ["console.log"], //移除console
+          },
+        },
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: OptimizeCssAssetsPlugin.ExtractTextPlugin.extract(
+          "style-loader",
+          "css-loader"
+        ),
+      },
+      {
+        test: /\.js$/,
+        // use: ['babel-loader?cacheDirectory'] 之前是使用这种方式直接使用 loader
+        // 现在用下面的方式替换成 happypack/loader，并使用 id 指定创建的 HappyPack 插件
+        use: ["happypack/loader?id=babel"],
+        // 排除 node_modules 目录下的文件
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new BundleAnalyzerPlugin(),
+    new OptimizeCssAssetsPlugin.ExtractTextPlugin("styles.css"),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.optimize\.css$/g,
+      cssProcessor: require("cssnano"),
+      cssProcessorPluginOptions: {
+        preset: ["default", { discardComments: { removeAll: true } }],
+      },
+      canPrint: true,
+    }),
+  ],
+};
